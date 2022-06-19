cead7a609e8115601edf0c6ae485bbf1aff8577e
ngicks <yknt.bsl@gmail.com>
2022-06-19 16:18:03 +0000

    docs(task_storage): add doc comments

--- task_storage/repository.go
+++ task_storage/repository.go
@@ -6,21 +6,37 @@ import (
 )
 
 type StateUpdater interface {
+	// UpdateState updates task State to new if current state is old.
+	// swapped is true if update is successful, false otherwise.
 	UpdateState(taskId string, old, new TaskState) (swapped bool, err error)
 }
 
 type Updater interface {
+	// Update updates the info with diff.
+	// Implementation may limit updatable state.
+	// If state is not updatable, return wrapped or unwrapped ErrNotUpdatableState.
 	Update(taskId string, diff UpdateDiff) (err error)
 }
 
 type Repository interface {
 	// Insert inserts the model to the repository.
-	// Id and LastModified field of passed TaskInfo are ignored.
+	// Id and LastModified field of passed TaskInfo are ignored and will be newly created.
 	Insert(TaskInfo) (taskId string, err error)
-	GetUpdatedAfter(time.Time) ([]TaskInfo, error)
+	// GetUpdatedAfter fetches all elements modified after t.
+	// Implementation may limit the number of fetched elements.
+	GetUpdatedAfter(t time.Time) ([]TaskInfo, error)
+	// GetById fetches an element associated with the id.
+	// If the id does not exist in the repository,
+	// GetById reports it by returning wrapped  or unwrapped ErrNoEnt error.
 	GetById(id string) (TaskInfo, error)
+	// MarkAsDone marks the task as done.
+	// Only Initialized or Working state must be updated to done state.
 	MarkAsDone(id string) (ok bool, err error)
+	// MarkAsCancelled marks the task as cancelled.
+	// Only Initialized or Working state must be updated to done state.
 	MarkAsCancelled(id string) (ok bool, err error)
+	// MarkAsFailed marks the task as failed which means workers failed to do this task.
+	// Only Initialized or Working state must be updated to done state.
 	MarkAsFailed(id string) (ok bool, err error)
 }
 
