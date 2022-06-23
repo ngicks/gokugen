cdad938c9bbf4c782b8344459c2ff50d055f3244
ngicks <yknt.bsl@gmail.com>
2022-06-23 19:00:38 +0000

    feat(middleware/log): logger middleware

---
+++ middleware/log/log.go
@@ -0,0 +1,37 @@
+package log
+
+import (
+	"time"
+
+	"github.com/ngicks/gokugen"
+)
+
+type Logger interface {
+	Info(taskId, workId string, v any)
+	Error(taskId, workId string, e error)
+}
+
+type LogMiddleware struct {
+	logger Logger
+}
+
+func (mw LogMiddleware) Middleware(handler gokugen.ScheduleHandlerFn) gokugen.ScheduleHandlerFn {
+	return func(ctx gokugen.SchedulerContext) (gokugen.Task, error) {
+		return handler(gokugen.WithWorkFnWrapper(
+			ctx,
+			func(self gokugen.SchedulerContext, workFn gokugen.WorkFn) gokugen.WorkFn {
+				return func(ctxCancelCh, taskCancelCh <-chan struct{}, scheduled time.Time) (any, error) {
+					taskId, _ := gokugen.GetTaskId(self)
+					workId, _ := gokugen.GetWorkId(self)
+					ret, err := workFn(ctxCancelCh, taskCancelCh, scheduled)
+					if err != nil {
+						mw.logger.Error(taskId, workId, err)
+					} else {
+						mw.logger.Info(taskId, workId, ret)
+					}
+					return ret, err
+				}
+			},
+		))
+	}
+}
