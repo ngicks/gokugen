ea40b4440a61854c17f099daaf232001b0ec5068
ngicks <yknt.bsl@gmail.com>
2022-07-04 13:26:23 +0000

    refactor(exmple): demonstrate unmarshaller registry

--- example/persistent_shceduler/main.go
+++ example/persistent_shceduler/main.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/ngicks/gokugen"
 	"github.com/ngicks/gokugen/impl/repository"
+	workregistry "github.com/ngicks/gokugen/impl/work_registry"
 	"github.com/ngicks/gokugen/scheduler"
 	taskstorage "github.com/ngicks/gokugen/task_storage"
 	syncparam "github.com/ngicks/type-param-common/sync-param"
@@ -29,33 +31,75 @@ func printJsonIndent(j any) {
 	fmt.Println(string(b))
 }
 
-func printNowWithId(workId string) gokugen.WorkFnWParam {
-	return func(taskCtx context.Context, scheduled time.Time, param any) (any, error) {
-		now := time.Now()
-		var isCtxCancelled bool
-		select {
-		case <-taskCtx.Done():
-			isCtxCancelled = true
-		default:
+type Param1 struct {
+	Foo string
+	Bar string
+}
+
+func (p Param1) Mix() string {
+	var ret string
+	for range p.Foo {
+		for range p.Bar {
+			ret += string(p.Foo[rand.Int63n(int64(len(p.Foo)))])
+			ret += string(p.Bar[rand.Int63n(int64(len(p.Bar)))])
 		}
+	}
+	return ret
+}
 
-		fmt.Printf(
-			"workId: %s, scheduled: %s, diff to now: %s, isCtxCancelled: %t, param: %v\n",
-			workId,
-			scheduled.Format(time.RFC3339Nano),
-			now.Sub(scheduled).String(),
-			isCtxCancelled,
-			param,
-		)
+type Param2 struct {
+	Baz string
+	Qux string
+}
 
+func (p Param2) Double() string {
+	var runes []rune
+	for _, bRune := range p.Baz {
+		for _, qRune := range p.Qux {
+			runes = append(runes, bRune)
+			runes = append(runes, qRune)
+		}
+	}
+	return string(runes)
+}
+
+func printNowWithIdParam1(workId string) gokugen.WorkFnWParam {
+	return func(taskCtx context.Context, scheduled time.Time, param any) (any, error) {
+		printer(workId, taskCtx, scheduled, param.(Param1).Mix())
 		return nil, nil
 	}
+}
+
+func printNowWithIdParam2(workId string) gokugen.WorkFnWParam {
+	return func(taskCtx context.Context, scheduled time.Time, param any) (any, error) {
+		printer(workId, taskCtx, scheduled, param.(Param2).Double())
+		return nil, nil
+	}
+}
+
+func printer(workId string, taskCtx context.Context, scheduled time.Time, param string) {
+	now := time.Now()
+	var isCtxCancelled bool
+	select {
+	case <-taskCtx.Done():
+		isCtxCancelled = true
+	default:
+	}
+
+	fmt.Printf(
+		"workId: %s, scheduled: %s, diff to now: %s, isCtxCancelled: %t, param: %s\n",
+		workId,
+		scheduled.Format(time.RFC3339Nano),
+		now.Sub(scheduled).String(),
+		isCtxCancelled,
+		param,
+	)
 }
 
 func prepare(dbFilename string) (
 	sched *gokugen.MiddlewareApplicator[*scheduler.Scheduler],
 	repo *repository.Sqlite3Repo,
-	workRegistory *syncparam.Map[string, gokugen.WorkFnWParam],
+	workRegistory taskstorage.WorkRegistry,
 	taskStorage *taskstorage.SingleNodeTaskStorage,
 	err error,
 ) {
@@ -63,10 +107,17 @@ func prepare(dbFilename string) (
 	if err != nil {
 		return
 	}
-	workRegistory = &syncparam.Map[string, gokugen.WorkFnWParam]{}
-	workRegistory.Store("func1", printNowWithId("func1"))
-	workRegistory.Store("func2", printNowWithId("func2"))
-	workRegistory.Store("func3", printNowWithId("func3"))
+	innerRegistry := &syncparam.Map[string, gokugen.WorkFnWParam]{}
+	innerRegistry.Store("func1", printNowWithIdParam1("func1"))
+	innerRegistry.Store("func2", printNowWithIdParam2("func2"))
+	innerRegistry.Store("func3", printNowWithIdParam2("func3"))
+
+	unmarshallerResgitry := &workregistry.AnyUnmarshaller{}
+	workregistry.AddType[Param1]("func1", unmarshallerResgitry)
+	workregistry.AddType[Param2]("func2", unmarshallerResgitry)
+	workregistry.AddType[Param2]("func3", unmarshallerResgitry)
+
+	workRegistory = workregistry.NewParamUnmarshaller(innerRegistry, unmarshallerResgitry)
 
 	sched = gokugen.NewMiddlewareApplicator(scheduler.NewScheduler(5, 0))
 
@@ -74,18 +125,7 @@ func prepare(dbFilename string) (
 		repo,
 		func(ti taskstorage.TaskInfo) bool { return true },
 		workRegistory,
-		func(sc gokugen.SchedulerContext) gokugen.SchedulerContext {
-			param, _ := gokugen.GetParam(sc)
-			return gokugen.WrapContext(
-				sc,
-				gokugen.WithParam(
-					map[string]any{
-						"synced":   "yayyay",
-						"paramOld": param,
-					},
-				),
-			)
-		},
+		nil,
 	)
 	sched.Use(taskStorage.Middleware(true)...)
 
@@ -112,25 +152,25 @@ func _main() (err error) {
 			nil,
 			nil,
 			gokugen.WithWorkId("func1"),
-			gokugen.WithParam([]string{"param", "param"}),
+			gokugen.WithParam(Param1{Foo: "param", Bar: "qqq"}),
 		),
 	)
 	sched.Schedule(
 		gokugen.BuildContext(
-			now,
+			now.Add(1*time.Second),
 			nil,
 			nil,
 			gokugen.WithWorkId("func2"),
-			gokugen.WithParam([]string{"param", "param"}),
+			gokugen.WithParam(Param2{Baz: "baaz", Qux: "param"}),
 		),
 	)
 	sched.Schedule(
 		gokugen.BuildContext(
-			now,
+			now.Add(3*time.Second),
 			nil,
 			nil,
 			gokugen.WithWorkId("func3"),
-			gokugen.WithParam([]string{"param", "param"}),
+			gokugen.WithParam(Param2{Baz: "quuux", Qux: "coorrge"}),
 		),
 	)
 
@@ -160,8 +200,11 @@ func _main() (err error) {
 	fmt.Println("restoed from persistent data storage:")
 	printJsonIndent(rescheduled)
 	printJsonIndent(schedulingErr)
+	for k, v := range rescheduled {
+		fmt.Println(k, v.GetScheduledTime())
+	}
 
-	ctx, cancel = context.WithDeadline(context.Background(), now.Add(7*time.Second))
+	ctx, cancel = context.WithDeadline(context.Background(), now.Add(5*time.Second))
 	sched.Scheduler().Start(ctx)
 	cancel()
 	sched.Scheduler().End()
