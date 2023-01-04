9ce94fbcf2faf49bf23816b366619a3c9a1a4616
ngicks <yknt.bsl@gmail.com>
2023-01-04 16:28:29 +0000

    docs(scheduler): add some notes

--- scheduler/util/drop_nanos.go
+++ scheduler/util/drop_nanos.go
@@ -6,6 +6,12 @@ import "time"
 
 // DropNanos drops nano seconds from t.
 func DropNanos(t time.Time) time.Time {
+	// TODO: use more specialized implementation?
+	//```go
+	// t.Add(-time.Duration(t.Nanosecond() % 1e6))
+	//```
+	// This is basically same as t.Truncate but less if's and switch's.
+	// But above impl does not remove a monotonic clock record.
 	return t.Truncate(time.Millisecond)
 }
 
