14f37590027d042760a2cd3d950bd09305ea0205
ngicks <yknt.bsl@gmail.com>
2023-01-21 14:06:42 +0000

    test(repository): add reconnect test

--- repository/gorm_test.go
+++ repository/gorm_test.go
@@ -7,27 +7,31 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	acceptancetest "github.com/ngicks/gokugen/scheduler/acceptance_test"
+	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-const inMemoryDb = ":memory:"
+const inMemoryDb = "file::memory:?cache=shared"
 
-func TestGormAcceptance(t *testing.T) {
-	var sqliteFilename string
-
+func getDbFilename() string {
 	if os.Getenv("GOKUGEN_SQLITE3_INMEMORY") != "" {
-		sqliteFilename = inMemoryDb
+		return inMemoryDb
 	} else {
 		dir, err := os.MkdirTemp(os.TempDir(), "test-gokugen-gorm-sqlite3-*")
 		if err != nil {
 			panic(err)
 		}
 
-		// in-memory sqlite3 db seemingly takes much power. Some CI env become slow with it.
-		sqliteFilename = filepath.Join(dir, "sqlite3.db")
+		// in-memory sqlite3 db seemingly causes some CI env slow. Default is an ordinary disk file in temp dir.
+		return filepath.Join(dir, "sqlite3.db")
 	}
+}
+
+func TestGormAcceptance(t *testing.T) {
+	sqliteFilename := getDbFilename()
 
 	t.Logf("%s", sqliteFilename)
 
@@ -59,4 +63,38 @@ func TestGormAcceptance(t *testing.T) {
 	}
 
 	acceptancetest.TestRepository(t, repo)
+}
+
+func TestGormReconnect(t *testing.T) {
+	require := require.New(t)
+
+	sqliteFilename := getDbFilename()
+
+	t.Logf("%s", sqliteFilename)
+
+	defer func() {
+		if sqliteFilename != inMemoryDb {
+			os.Remove(sqliteFilename)
+		}
+	}()
+
+	repo, err := NewSqlite3(sqliteFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	tasks, err := addRandomTask(repo, 100)
+	require.NoError(err)
+
+	reconnected, err := NewSqlite3(sqliteFilename)
+	require.NoError(err)
+
+	for _, task := range tasks {
+		got, err := reconnected.GetById(task.Id)
+		require.NoError(err)
+
+		if diff := cmp.Diff(task, got); diff != "" {
+			t.Fatalf("not equal: %s", diff)
+		}
+	}
 }
