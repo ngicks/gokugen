0d2f5823fb335ae43d43cbca811c74c7a7575468
ngicks <yknt.bsl@gmail.com>
2023-01-21 18:18:27 +0000

    refactor(repository): auto migration is only called in NewSqlite3

--- repository/gorm.go
+++ repository/gorm.go
@@ -9,16 +9,16 @@ import (
 func NewGorm(db *gorm.DB, hookTimer HookTimer) (*Repository, error) {
 	core := NewDefaultGormCore(db)
 
-	err := core.db.AutoMigrate(&gormtask.GormTask{})
-	if err != nil {
-		return nil, err
-	}
-
 	return New(core, hookTimer), nil
 }
 
 func NewSqlite3(dbPath string, opts ...gorm.Option) (*Repository, error) {
 	db, err := gorm.Open(sqlite.Open(dbPath), opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&gormtask.GormTask{})
 	if err != nil {
 		return nil, err
 	}
